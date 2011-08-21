e1dcba60690ffdb7534b4f41a49bf9e30b737d23
fangyuan <fangyuan>
2011-08-22 00:16:33 +0800

    a simple game engine framework

---
+++ src/canvas/game-framework/main.js
@@ -0,0 +1,14 @@
+var FPS=30;
+
+var SECONDS_BETWEEN_FRAMES=1/FPS;
+
+var g_GameObjectManager=null;
+
+var g_image=new Image();
+g_image.src="smiley.jpg";
+
+window.onload=init;
+
+function init(){
+	new GameObjectManager().startupGameObjectManager();
+}
